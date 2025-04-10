8aa10cd09040a72a275785bd687e94fd0e5d9e65
maksym_solo <solmaks05@gmail.com>
2025-04-10 15:08:05 +0300

    settings page was done, file selection was finished

--- screens/program_screen.py
+++ screens/program_screen.py
@@ -1,48 +1,36 @@
 from kivy.uix.screenmanager import Screen
 from kivy.properties import StringProperty
+from kivy.uix.filechooser import FileChooserListView
+from kivy.uix.popup import Popup
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+import csv
 
-low_mode_temp = 20
-default_mode_temp = 40
-high_mode_temp = 80
+upladder_mode_temp = 20
+downladder_mode_temp = 40
+heating_mode_temp = 80
 
-low_mode_time = 12
-default_mode_time = 14
-high_mode_time = 16
+upladder_mode_time = 12
+downladder_mode_time = 14
+heating_mode_time = 16
 
 class ProgramScreen(Screen):
-    low_mode_temp_text = StringProperty(str(low_mode_temp))
-    default_mode_temp_text = StringProperty(str(default_mode_temp))
-    high_mode_temp_text = StringProperty(str(high_mode_temp))
-    low_mode_time_text = StringProperty(str(low_mode_time))
-    default_mode_time_text = StringProperty(str(default_mode_time))
-    high_mode_time_text = StringProperty(str(high_mode_time))
+    upladder_mode_temp_text = StringProperty(str(upladder_mode_temp))
+    downladder_mode_temp_text = StringProperty(str(downladder_mode_temp))
+    heating_mode_temp_text = StringProperty(str(heating_mode_temp))
+    upladder_mode_time_text = StringProperty(str(upladder_mode_time))
+    downladder_mode_time_text = StringProperty(str(downladder_mode_time))
+    heating_mode_time_text = StringProperty(str(heating_mode_time))
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.low_mode_temp_text = str(low_mode_temp)
-        self.low_mode_time_text = str(low_mode_time)
-        self.default_mode_temp_text = str(default_mode_temp)
-        self.default_mode_time_text = str(default_mode_time)
-        self.high_mode_time_text = str(high_mode_time)
-        self.high_mode_temp_text = str(high_mode_temp)
+        self.upladder_mode_temp_text = str(upladder_mode_temp)
+        self.upladder_mode_time_text = str(upladder_mode_time)
+        self.downladder_mode_temp_text = str(downladder_mode_temp)
+        self.downladder_mode_time_text = str(downladder_mode_time)
+        self.heating_mode_time_text = str(heating_mode_time)
+        self.heating_mode_temp_text = str(heating_mode_temp)
 
-    # Action functions for each button press
     def go_back(self):
-        self.manager.transition.direction = 'right'  # Example transition direction
-        self.manager.current = 'previous_screen'  # Replace with your previous screen name
-
-    def reorder(self):
-        # Implement reorder functionality here
-        print("Reordering...")
-
-    def start_simple_mode(self):
-        # Logic for simple mode
-        print("Starting Simple Mode with Temperature: 190°C and Duration: 12 mins")
-
-    def start_extended_mode(self):
-        # Logic for extended mode
-        print("Starting Extended Mode with Temperature: 50°C and Duration: 20 mins")
-
-    def start_low_temp_mode(self):
-        # Logic for low temperature mode
-        print("Starting Low Temp Mode with Temperature: 20°C and Duration: 3 mins")
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'second'
