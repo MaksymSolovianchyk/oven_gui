eb8c6f362cdf7ecbad7e51112b196c20c9a57a1f
maksym_solo <solmaks05@gmail.com>
2025-04-11 14:48:14 +0300

    upladderscreen was implemented

--- screens/program_screen.py
+++ screens/program_screen.py
@@ -2,6 +2,7 @@ from kivy.uix.screenmanager import Screen
 from kivy.properties import StringProperty
 
 
+
 upladder_mode_temp = 20
 downladder_mode_temp = 40
 heating_mode_temp = 80
@@ -9,6 +10,9 @@ heating_mode_temp = 80
 upladder_mode_time = 12
 downladder_mode_time = 14
 heating_mode_time = 16
+temp = int
+time = int
+
 
 class ProgramScreen(Screen):
     upladder_mode_temp_text = StringProperty(str(upladder_mode_temp))
@@ -27,10 +31,29 @@ class ProgramScreen(Screen):
         self.heating_mode_time_text = str(heating_mode_time)
         self.heating_mode_temp_text = str(heating_mode_temp)
 
+
     def go_back(self):
         self.manager.transition.direction = 'right'
         self.manager.current = 'second'
 
-    def go_to_up_ladder_screen(self):
+    def go_to_ladder_screen(self, mode):
+        if mode == 'up_ladder':
+            temp = upladder_mode_temp
+            time = upladder_mode_time
+            graph = 'logos/up_ladder.png'
+        elif mode == 'down_ladder':
+            temp = downladder_mode_temp
+            time = downladder_mode_time
+            graph = 'logos/donw_ladder.png'
+        elif mode == 'heating_ladder':
+            temp = heating_mode_temp
+            time = heating_mode_time
+            graph = 'logos/heating_mode.png'
+
+        up_screen = self.manager.get_screen('up_ladder_screen')
+        up_screen.temp_text = str(temp)
+        up_screen.time_text = str(time)
+        up_screen.graph_source = graph  # Set image
+
         self.manager.transition.direction = 'left'
         self.manager.current = 'up_ladder_screen'
--- screens/up_ladder_screen.py
+++ screens/up_ladder_screen.py
@@ -1,8 +1,19 @@
 from kivy.uix.screenmanager import Screen
 from kivy.properties import StringProperty
+from screens.program_screen import time, temp
+
 
 class UpLadderScreen(Screen):
+    temp_text = StringProperty("")
+    time_text = StringProperty("")
+    graph_source = StringProperty("")
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def go_back(self):
         self.manager.transition.direction = 'right'
-        self.manager.current = 'second'
+        self.manager.current = 'program_screen'
+
+    def run(self):
+        self.manager.transition.direction = 'left'
+        self.manager.current = 'run_screen'
